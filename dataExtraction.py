38189c5ab4f44a6500e1f72eb64e8e54efc6ae8b
MelisaRS <melisa.rs.1990@gmail.com>
2023-11-22 12:40:08 -0400

    Changes in Data Extraction: names and removal of current directory

--- dataExtraction.py
+++ dataExtraction.py
@@ -4,8 +4,8 @@ import os
 from langchain.document_loaders import PyPDFLoader
 
 # Crea la carpeta DataTxt donde estara los txt de cada pdf
-def folderCreator  (currDirectory, outputFolder):
-    ruta_carpeta_cases = os.path.join(currDirectory, outputFolder)
+def folderCreator  (output_folder):
+    ruta_carpeta_cases = os.path.join(output_folder)
 
     if not os.path.exists(ruta_carpeta_cases):
         os.makedirs(ruta_carpeta_cases)
@@ -18,13 +18,13 @@ def pdfLoader(path,pdfName):
     return loader.load()
 
 # Crea el txt para cada pdf
-def creatorTxt (currDirectory, subDirectory, outputDirectory) :
+def creatorTxt (input_folder_PDF, output_folder) :
 
-    cases = os.listdir(currDirectory + subDirectory)
+    cases = os.listdir(input_folder_PDF)
     pdfDocs_cases = []
 
     for case in cases:
-        pdfDocs_cases.append(pdfLoader(currDirectory + subDirectory,  case ))
+        pdfDocs_cases.append(pdfLoader(input_folder_PDF,  case ))
 
     for i, doc_SC in enumerate(pdfDocs_cases):
         pdf_name = cases[i]
@@ -32,7 +32,7 @@ def creatorTxt (currDirectory, subDirectory, outputDirectory) :
         # Elimina la extensión .pdf del nombre del archivo
         pdf_name = os.path.splitext(pdf_name)[0]
 
-        doc_path = folderCreator (currDirectory, outputDirectory)
+        doc_path = folderCreator (output_folder)
 
         text = ""
         
@@ -44,20 +44,20 @@ def creatorTxt (currDirectory, subDirectory, outputDirectory) :
         with open(os.path.join(doc_path, f"{pdf_name}.txt"), "w", encoding="utf-8") as file:
             file.write(text)
         
-def extractorStudyCases (currDirectory, subDirectoryForLoad, output_Directory):
+def extractorStudyCases (input_folder_PDF, output_folder):
     
-    folderCreator (currDirectory, output_Directory)
-    creatorTxt (currDirectory, subDirectoryForLoad , output_Directory)
+    folderCreator (output_folder)
+    creatorTxt (input_folder_PDF, output_folder)
 
-    print("------------------------------------------------------")
-    print("You have extracted and converted your PDF files to txt")
-    print("------------------------------------------------------")
+    print("-------------------------------------------------------------------------------")
+    print(f"You have extracted and converted your PDF files to txt inside: {output_folder}")
+    print("-------------------------------------------------------------------------------")
 
 ###
 ##### VARIABLES Y FUNCION PARA CORRER EL DATAEXTRACTOR #####
-current_directory = os.path.dirname(os.path.abspath(__file__))
-subDirectoryLoad = '/Data/StudyCases/'
-directoryOutput = 'StudyCases'
+#current_directory = os.path.dirname(os.path.abspath(__file__))
+input_folder_PDF = './Data/StudyCases/'
+output_folder = 'StudyCases'
 
-extractorStudyCases (current_directory, subDirectoryLoad, directoryOutput)
+extractorStudyCases (input_folder_PDF, output_folder)
 
